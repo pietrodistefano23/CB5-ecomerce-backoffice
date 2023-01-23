683815b5912303828d0f9efd7664f2ad6f5f698b
pietrodistefano23 <pietrodistefano2306@gmail.com>
2023-01-23 18:22:16 +0100

    add initial layout

---
+++ ecommerce-backoffice/src/components/main/Main.jsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss'
+const Main = () => {
+
+    return (
+        <div className={styles.Main}>
+            Main
+        </div>
+    )
+} 
+
+export default Main ;
